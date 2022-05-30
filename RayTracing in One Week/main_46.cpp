b5ae3fd88505d7c944fcac55d30d2e0b59fab6b8
shirlenesky <2411038489@qq.com>
2022-05-30 11:04:37 +0800

    image texture

--- RayTracing in One Week/main_46.cpp
+++ RayTracing in One Week/main_46.cpp
@@ -109,10 +109,10 @@ hittable_list two_perlin_spheres()
 
 hittable_list earth()
 {
-	auto earth_texture = make_shared<image_texture>("./HK.jpg");
+	auto earth_texture = make_shared<image_texture>("HK.jpg");
 	auto earth_surface = make_shared<lambertian>(earth_texture);
 	auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
-
+	
 	return hittable_list(globe);
 }
 
@@ -147,7 +147,7 @@ int main()
 		lookfrom = point3(13, 2, 3);
 		lookat = point3(0, 0, 0);
 		vfov = 20.0;
-		break;
+		break;	
 	case 3:
 		world = two_perlin_spheres();
 		lookfrom = point3(13, 2, 3);
