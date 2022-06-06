b141c70314a74fc775e796d977d1c83778e8bc69
shirlenesky <2411038489@qq.com>
2022-06-06 17:26:58 +0800

    8-displacement

--- RayTracing in One Week/main.cpp
+++ RayTracing in One Week/main.cpp
@@ -8,6 +8,7 @@
 #include "moving_sphere.h"
 #include "texture.h"
 #include "aarect.h"
+#include "box.h"
 
 #include<iostream>
 using namespace std;
@@ -160,15 +161,24 @@ hittable_list cornell_box()
 	auto red = make_shared<lambertian>(color(0.65, 0.05, 0.05));
 	auto white = make_shared<lambertian>(color(0.73, 0.73, 0.73));
 	auto green = make_shared<lambertian>(color(0.12, 0.45, 0.15));
-	//auto light = make_shared<diffuse_light>(color(15, 15, 15));	
-	auto difflight = make_shared<diffuse_light>(color(5, 5, 5));
+	auto light = make_shared<diffuse_light>(color(15, 15, 15));	
 
 	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
 	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
-	objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, difflight));
+	objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
 	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
 	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+
+	shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+	box1 = make_shared<rotate_y>(box1, 15);
+	box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+	objects.add(box1);
+	shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
+	box2 = make_shared<rotate_y>(box2, -18);
+	box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+	objects.add(box2);
+
 	return objects;
 }
 
@@ -178,7 +188,7 @@ int main()
 	auto aspect_ratio = 16.0 / 9.0;
 	int image_width = 400;
 	int samples_per_pixel = 100;
-	int max_depth = 20;
+	int max_depth = 30;
 	
 
 	//World
@@ -236,7 +246,7 @@ int main()
 		world = cornell_box();
 		aspect_ratio = 1.0;
 		image_width = 600;
-		samples_per_pixel = 50;
+		samples_per_pixel = 100;
 		lookfrom = point3(278, 278, -800);
 		lookat = point3(278, 278, 0);
 		vfov = 40.0;
